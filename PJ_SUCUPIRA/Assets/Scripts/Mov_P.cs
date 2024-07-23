1a8d54a610cd1a6467b0bbbadfc2cf541da99119
EVERSON <124588837+NETHER2B@users.noreply.github.com>
2024-07-23 16:55:07 -0300

    second

--- PJ_SUCUPIRA/Assets/Scripts/Mov_P.cs
+++ PJ_SUCUPIRA/Assets/Scripts/Mov_P.cs
@@ -12,6 +12,12 @@ public class Mov_P : MonoBehaviour
     public bool sensor;
     public Transform posicaosensor;
     private Animator anim;
+    public bool verifdirecao;
+
+    public GameObject municao;
+    public Transform posicaoTiro;
+    public float speedtiro;
+
 
 
     
@@ -35,10 +41,42 @@ public class Mov_P : MonoBehaviour
         }
        
         anim.SetInteger("run",(int)movHorizontal);
+        anim.SetBool("jump", sensor);
+        
+        if(movHorizontal>0 && verifdirecao == true)
+        {
+            direcao();
+            speedtiro *= -1;
+        }
+        if(movHorizontal<0 && verifdirecao == false)
+        {
+            direcao();
+            speedtiro *= -1;
+        }
+        if(Input.GetMouseButtonDown(0))
+        {
+            atira();
+            
+        }
     }
 
     public void verificar()
     {
-        sensor = Physics2D.OverlapCircle(posicaosensor.position, 0.34f);
+        sensor = Physics2D.OverlapCircle(posicaosensor.position, 0.2f);
+    }
+    public void direcao()
+    {
+      verifdirecao = !verifdirecao;
+        float x = transform.localScale.x * -1;
+        
+        transform.localScale = new Vector3(x, transform.localScale.y,transform.localScale. z);
+        municao.GetComponent<SpriteRenderer>().flipX = verifdirecao;
+    }
+    public void atira()
+    {
+        GameObject temp = Instantiate(municao);
+        temp.transform.position = posicaoTiro.position;
+        temp.GetComponent<Rigidbody2D>().velocity = new Vector2(speedtiro, 0);
+        anim.SetTrigger("shoot");
     }
 }
